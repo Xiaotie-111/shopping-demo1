f8abc72ea80aaa67d5e6c75c616d8cca200c13a5
小铁-111 <2091055092@qq.com>
2025-12-02 10:52:33 +0800

    软件需求分析附件A已修改

---
+++ src/main/java/edu/ngd/eurke/shoppingdemo1/ShoppingDemo1Application.java
@@ -0,0 +1,13 @@
+package edu.ngd.eurke.shoppingdemo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingDemo1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShoppingDemo1Application.class, args);
+    }
+
+}
---
+++ src/test/java/edu/ngd/eurke/shoppingdemo1/ShoppingDemo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.ngd.eurke.shoppingdemo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingDemo1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
