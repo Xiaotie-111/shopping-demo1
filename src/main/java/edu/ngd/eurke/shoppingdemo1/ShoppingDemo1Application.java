09bdfa82f2395bf5161f7cf7921b5f72c5a2aa64
Xiaotie-111 <2091055092@qq.com>
2025-12-11 13:15:54 +0800

    界面功能修改

--- src/main/java/edu/ngd/eurke/shoppingdemo1/ShoppingDemo1Application.java
+++ src/main/java/edu/ngd/eurke/shoppingdemo1/ShoppingDemo1Application.java
@@ -2,12 +2,14 @@ package edu.ngd.eurke.shoppingdemo1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"edu.ngd.eurke.shoppingdemo1", "edu.ngd.platform"})
 public class ShoppingDemo1Application {
 
-    public static void main(String[] args) {
-        SpringApplication.run(ShoppingDemo1Application.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingDemo1Application.class, args);
+	}
 
 }
