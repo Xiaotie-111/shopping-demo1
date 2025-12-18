25c82862a973d273b169002cfbad627ef8e43b05
Xiaotie-111 <2091055092@qq.com>
2025-12-18 10:26:50 +0800

    所有功能全部实现

---
+++ src/main/java/edu/ngd/platform/mapper/OrderItemMapper.java
@@ -0,0 +1,10 @@
+package edu.ngd.platform.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import edu.ngd.platform.model.OrderItem;
+
+/**
+ * 订单项Mapper接口
+ */
+public interface OrderItemMapper extends BaseMapper<OrderItem> {
+}
---
+++ src/main/java/edu/ngd/platform/mapper/OrderMapper.java
@@ -0,0 +1,10 @@
+package edu.ngd.platform.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import edu.ngd.platform.model.Order;
+
+/**
+ * 订单Mapper接口
+ */
+public interface OrderMapper extends BaseMapper<Order> {
+}
